c10852f129a5df77d433866c284a94cf058bf3d8
Fuyunwang <2375872953@qq.com>
2017-07-31 22:58:56 +0800

    项目初始化,包名完善

--- app/src/main/java/com/beautifulsoup/dancego/MainActivity.java
+++ app/src/main/java/com/beautifulsoup/dancego/MainActivity.java
@@ -3,6 +3,7 @@ package com.beautifulsoup.dancego;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
